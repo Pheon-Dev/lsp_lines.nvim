fa70b3ac432bc3062922a2698acf1b3a73a78592
Pheon-Dev <devpheon@gmail.com>
2023-01-25 12:28:24 +0300

    feat: porting data

---
+++ README.md
@@ -0,0 +1,77 @@
+# lsp_lines.nvim
+
+`lsp_lines` is a simple neovim plugin that renders diagnostics using virtual
+lines on top of the real line of code.
+
+![A screenshot of the plugin in action](screenshot.png)
+
+# Background
+
+LSPs provide lots of useful diagnostics for code (typically: errors, warnings,
+linting). By default they're displayed using virtual text at the end of the
+line which is in many cases good enough, but often there's more than one
+diagnostic per line, or there's a very long diagnostic, and there's no handy
+way to read the whole thing.
+
+`lsp_lines` seeks to solve this issue.
+
+# Development
+
+This works well in its current state. Please report any issues you may find.
+
+I've considered using the normal virtual text for all diagnostics and only
+using virtual lines for the currently focused line, but that requires some
+extra work I haven't had the time for.
+
+# Installation
+
+Using packer.nvim (should probably be registered _after_ `lspconfig`):
+
+```lua
+use({
+  "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  config = function()
+    require("lsp_lines").register_lsp_virtual_lines()
+  end,
+})
+```
+
+It's recommended to also remove the regular virtual text diagnostics to avoid
+pointless duplication:
+
+```lua
+-- Disable virtual_text since it's redundant due to lsp_lines.
+vim.diagnostic.config({
+  virtual_text = false,
+})
+```
+
+# Configuration
+
+This plugin's functionality can be disabled with:
+
+```lua
+vim.diagnostic.config({ virtual_lines = false })
+```
+
+And it can be re-enabled via:
+
+```lua
+vim.diagnostic.config({ virtual_lines = true })
+```
+
+The prefix icon shown to the left of diagnostics can be configured with:
+
+```lua
+vim.diagnostic.config({ virtual_lines = { prefix = "🔥" } })
+```
+
+# Contributing
+
+- Discussion or patches: ~whynothugo/lsp_lines.nvim@lists.sr.ht
+- Issues: https://todo.sr.ht/~whynothugo/lsp_lines.nvim
+- Tips: https://ko-fi.com/whynothugo
+
+# Licence
+
+This project is licensed under the ISC licence. See LICENCE for more details.
---
+++ lua/lsp_lines/init.lua
@@ -0,0 +1,149 @@
+local M = {}
+
+local highlight_groups = {
+  [vim.diagnostic.severity.ERROR] = "DiagnosticVirtualTextError",
+  [vim.diagnostic.severity.WARN] = "DiagnosticVirtualTextWarn",
+  [vim.diagnostic.severity.INFO] = "DiagnosticVirtualTextInfo",
+  [vim.diagnostic.severity.HINT] = "DiagnosticVirtualTextHint",
+}
+
+local SPACE = 0
+local DIAGNOSTIC = 1
+local OVERLAP = 2
+
+-- Registers a wrapper-handler to render lsp lines.
+-- This should usually only be called once, during initialisation.
+M.register_lsp_virtual_lines = function()
+  -- TODO: When a diagnostic changes for the current line, the cursor is not shifted properly.
+  -- TODO: On LSP restart (e.g.: diagnostics cleared), errors don't go away.
+
+  vim.diagnostic.handlers.virtual_lines = {
+    ---@param namespace number
+    ---@param bufnr number
+    ---@param diagnostics table
+    ---@param opts boolean
+    show = function(namespace, bufnr, diagnostics, opts)
+      vim.validate({
+        namespace = { namespace, "n" },
+        bufnr = { bufnr, "n" },
+        diagnostics = {
+          diagnostics,
+          vim.tbl_islist,
+          "a list of diagnostics",
+        },
+        opts = { opts, "t", true },
+      })
+
+      table.sort(diagnostics, function(a, b)
+        if a.lnum ~= b.lnum then
+          return a.lnum < b.lnum
+        else
+          return a.col < b.col
+        end
+      end)
+
+      local ns = vim.diagnostic.get_namespace(namespace)
+      if not ns.user_data.virt_lines_ns then
+        ns.user_data.virt_lines_ns = vim.api.nvim_create_namespace("")
+      end
+      local virt_lines_ns = ns.user_data.virt_lines_ns
+
+      vim.api.nvim_buf_clear_namespace(bufnr, virt_lines_ns, 0, -1)
+
+      local line_stacks = {}
+      local prev_lnum = -1
+      local prev_col = -1
+      for id, diagnostic in ipairs(diagnostics) do
+        diagnostic.id = id -- Tie the id to the actual object.
+
+        if line_stacks[diagnostic.lnum] == nil then
+          line_stacks[diagnostic.lnum] = {}
+        end
+
+        local stack = line_stacks[diagnostic.lnum]
+
+        if diagnostic.lnum ~= prev_lnum then
+          table.insert(stack, { SPACE, string.rep(" ", diagnostic.col) })
+          table.insert(stack, { DIAGNOSTIC, diagnostic })
+        elseif diagnostic.col ~= prev_col then
+          table.insert(stack, { SPACE, string.rep(" ", diagnostic.col - prev_col - 1) })
+          table.insert(stack, { DIAGNOSTIC, diagnostic })
+        else
+          table.insert(stack, { OVERLAP, diagnostic.severity })
+          table.insert(stack, { DIAGNOSTIC, diagnostic })
+        end
+
+        prev_lnum = diagnostic.lnum
+        prev_col = diagnostic.col
+      end
+
+      for lnum, lelements in pairs(line_stacks) do
+        local virt_lines = {}
+        for i = #lelements, 1, -1 do -- last element goes on top
+          if lelements[i][1] == DIAGNOSTIC then
+            local diagnostic = lelements[i][2]
+
+            local left = {}
+            local overlap = false
+
+            -- Iterate the stack for this line to find elements on the left.
+            for j = 1, i - 1 do
+              local type = lelements[j][1]
+              local data = lelements[j][2]
+              if type == SPACE then
+                table.insert(left, { data, "" })
+              elseif type == DIAGNOSTIC then
+                -- If an overlap follows this, don't add an extra column.
+                if lelements[j + 1][1] ~= OVERLAP then
+                  table.insert(left, { "│", highlight_groups[data.severity] })
+                end
+                overlap = false
+              elseif type == OVERLAP then
+                overlap = true
+              end
+            end
+
+            local center
+            if overlap then
+              center = { { "├──── ", highlight_groups[diagnostic.severity] } }
+            else
+              center = { { "└──── ", highlight_groups[diagnostic.severity] } }
+            end
+
+            for msg_line in diagnostic.message:gmatch("([^\n]+)") do
+              local vline = {}
+              vim.list_extend(vline, left)
+              vim.list_extend(vline, center)
+              vim.list_extend(vline, { { msg_line, highlight_groups[diagnostic.severity] } })
+
+              table.insert(virt_lines, vline)
+
+              -- Special-case for continuation lines:
+              if overlap then
+                center = { { "│", highlight_groups[diagnostic.severity] }, { "     ", "" } }
+              else
+                center = { { "      ", "" } }
+              end
+            end
+          end
+        end
+
+        vim.api.nvim_buf_set_extmark(bufnr, virt_lines_ns, lnum, 0, {
+          id = lnum,
+          virt_lines = virt_lines,
+          virt_lines_above = false,
+        })
+      end
+    end,
+    ---@param namespace number
+    ---@param bufnr number
+    hide = function(namespace, bufnr)
+      local ns = vim.diagnostic.get_namespace(namespace)
+      if ns.user_data.virt_lines_ns then
+        vim.api.nvim_buf_clear_namespace(bufnr, ns.user_data.virt_lines_ns, 0, -1)
+      end
+    end,
+  }
+end
+
+return M
